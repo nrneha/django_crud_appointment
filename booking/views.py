16d712bf1d707f1600518aace69e8819661a2a5e
nrneha <nehanr96@gmail.com>
2025-03-07 11:44:29 +0530

    Initial Commit

---
+++ booking/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from . models import *
+
+def home(request):
+    data = TeamofDoctors.objects.all
+    return render(request,"booking/home.html",{'data':data})
+
+
+def appointment_page(request,doctor_id):
+    doctor = TeamofDoctors.objects.get(id=doctor_id)
+    tokens = Token.objects.filter(doctor=doctor,is_booked=False)
+    return render(request,"booking/appointment.html",{'doctor':doctor,'tokens':tokens})
